2af587bf4c2a06c9c6c996de4adaef45aab8556b
amirhossein <amirhossein_zebardast@yahoo.com>
2022-01-30 13:46:12 +0330

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'remixicon/fonts/remixicon.css'
+import './assets/index.css'
 
 createApp(App).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      "white":'#FFFFFF',
+      'bright-sun':'#FFD337',
+      "dark":'#181B35',
+      "light-slat-Blue":"#7B62FF",
+      "light-indigo":"#6754D7",
+      "Amethyst-smoke":"#9598BB",
+      "black-Eel":"#474138",
+      "cloud-burst":"#2A2E54",
+      "regent-grey":"#8C90B7",
+      "ebony-clay":"#1E2142",
+      "arty-click-crimson":"#FC0380",
+      "haiti":"#161A3A",
+      "melrose":"#CAC0FF",
+    },
+    fontFamily:{
+      sans:['Roboto',"sans-serif"]
+    }
+  },
+  plugins: [],
+}
