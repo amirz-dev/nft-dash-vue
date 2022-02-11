e0c64ac2c0ec0f0b3d9699d0318eda4833474ab6
Amirhossein Zebardast <51723926+amirz-dev@users.noreply.github.com>
2022-02-11 18:20:31 +0330

    Update user-wrapper.vue

--- src/components/theMenu/wrapper/user-wrapper.vue
+++ src/components/theMenu/wrapper/user-wrapper.vue
@@ -1,12 +1,16 @@
 <template>
    <div class="flex w-full gap-3">
       <div class="basis-1/4">
-        <img
+        
+      <!-- img -->
+      <img
           :src="require('@/assets/img/1.jpg')"
           alt=""
           class="w-16 rounded-full"
         />
       </div>
+      
+      <!-- name -->
       <div class="basis-2/4">
         <p class="text-[20px] xl:text-[24px] font-semibold text-[#131736] dark:text-white">Amir</p>
         <p class="text-[12px] xl:text-[14px] font-medium xl:font-semibold text-[#A0A2AE] dark:text-white">
@@ -17,8 +21,10 @@
           </span>
         </p>
       </div>
+      
+      <!-- Github -->
       <div class="basis-1/4 flex justify-end self-center">
-        <a href="https://github.com/amirz-dev" target="_blank" class="hover:cursor-pointer">
+        <a href="https://github.com/amirz-dev/nft-dash-vue" target="_blank" class="hover:cursor-pointer">
           <i class="ri-github-line text-[32px] text-light-indigo"></i>
         </a>
       </div>
